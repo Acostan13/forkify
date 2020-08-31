5a65647b65416ee261f58b18f03e7a6f2960b553
Alex Costan <acostan.con@orangetheory.com>
2020-08-31 08:41:55 -0400

    creating buttons for pagination

--- src/js/views/searchView.js
+++ src/js/views/searchView.js
@@ -49,6 +49,30 @@ const renderRecipe = (recipe) => {
   elements.searchResList.insertAdjacentHTML("beforeend", markup)
 }
 
-export const renderResults = (recipes) => {
-  recipes.forEach(renderRecipe)
+// type: 'prev' or 'next'
+const createButton = (page, type) => `
+    <button class="btn-inline results__btn--${type}">
+        <svg class="search__icon">
+            <use href="img/icons.svg#icon-triangle-${type === 'prev' ? 'left' : 'right'}"></use>
+        </svg>
+        <span>Page ${type === 'prev' ? page - 1 : page + 1}</span>
+    </button>
+`
+
+const renderButtons = (page, numResults, resPerPage) => {
+    const pages = Math.ceil(numResults / resPerPage)
+
+    if (page === 1 && pages > 1) {
+        // Button to go to the next page
+    } else if (page < pages) {
+        // Both buttons
+    } else if (page === pages && pages > 1) {
+        // Button to go to prev page
+    }
+}
+
+export const renderResults = (recipes, page = 1, resPerPage = 10) => {
+    const start = (page - 1) * resPerPage
+    const end = page * resPerPage
+    recipes.slice(start, end).forEach(renderRecipe)
 }
