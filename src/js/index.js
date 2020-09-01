396bfcc43ab2dddfd3e00ab17a15dcef29c47e52
Alex Costan <acostan.con@orangetheory.com>
2020-09-01 08:43:57 -0400

    building the recipe model

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,5 @@
 import Search from "./models/Search"
+import Recipe from './models/Recipe'
 import * as searchView from "./views/searchView"
 import { elements, renderLoader, clearLoader } from "./views/base"
 
@@ -11,6 +12,9 @@ import { elements, renderLoader, clearLoader } from "./views/base"
 
 const state = {}
 
+/*
+ * Search Controller 
+ */
 const controlSearch = async () => {
   // 1) Get query from view
   const query = searchView.getInput()
@@ -46,3 +50,10 @@ elements.searchResPages.addEventListener('click', e => {
       searchView.renderResults(state.search.result, goToPage)
   }
 })
+
+/*
+ * Recipe Controller 
+ */
+const r = new Recipe(46956)
+r.getRecipe()
+console.log(r)
