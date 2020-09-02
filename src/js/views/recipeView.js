0430b67e2c4de703a421512d80582eec94ca5dc2
Alex Costan <acostan.con@orangetheory.com>
2020-09-02 15:11:04 -0400

    using npm package fractional to help format ingredient count

--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -1,7 +1,30 @@
 import { elements } from "./base"
+import { Fraction } from "fractional"
 
 export const clearRecipe = () => {
-    elements.recipe.innerHTML = ''
+  elements.recipe.innerHTML = ""
+}
+
+const formatCount = (count) => {
+  if (count) {
+    // Ex: count = 2.5 --> 5/2 --> 2 1/2
+    // Ex: count = 0.5 --> 1/2
+    const [int, dec] = count
+      .toString()
+      .split(".")
+      .map((el) => parseInt(el, 10))
+
+    if (!dec) return count
+
+    if (int === 0) {
+      const fr = new Fraction(count)
+      return `${fr.numerator}/${fr.denominator}`
+    } else {
+      const fr = new Fraction(count - int)
+      return `${int} ${fr.numerator}/${fr.denominator}`
+    }
+  }
+  return "?"
 }
 
 const createIngredient = (ingredient) => `
@@ -9,7 +32,7 @@ const createIngredient = (ingredient) => `
         <svg class="recipe__icon">
             <use href="img/icons.svg#icon-check"></use>
         </svg>
-        <div class="recipe__count">${ingredient.count}</div>
+        <div class="recipe__count">${formatCount(ingredient.count)}</div>
         <div class="recipe__ingredient">
             <span class="recipe__unit">${ingredient.unit}</span>
             ${ingredient.ingredient}
@@ -69,7 +92,7 @@ export const renderRecipe = (recipe) => {
 
     <div class="recipe__ingredients">
         <ul class="recipe__ingredient-list">
-            ${recipe.ingredients.map((el) => createIngredient(el)).join('')}
+            ${recipe.ingredients.map((el) => createIngredient(el)).join("")}
         </ul>
 
         <button class="btn-small recipe__btn">
