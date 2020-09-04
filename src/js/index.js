e098a9e3ea5992739ed4e4e51dbcae56397eac1f
Alex Costan <acostan.con@orangetheory.com>
2020-09-04 12:31:47 -0400

    building the likes controller

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,7 @@
 import Search from "./models/Search"
 import Recipe from "./models/Recipe"
 import List from "./models/List"
+import Likes from "./models/Likes"
 import * as searchView from "./views/searchView"
 import * as recipeView from "./views/recipeView"
 import * as listView from "./views/listView"
@@ -104,7 +105,6 @@ const controlRecipe = async () => {
 /*
  * List Controller
  */
-
 const controlList = () => {
   // Create a new list IF there is none yet
   if (!state.list) state.list = new List()
@@ -135,6 +135,41 @@ elements.shopping.addEventListener('click', e => {
   }
 })
 
+/*
+ * Recipe Controller
+ */
+const controlLike = () => {
+  if (!state.likes) state.likes = new Likes()
+  const currentID = state.recipe.id
+
+  // User has not yet liked current recipe
+  if (!state.likes.isLiked(currentID)) {
+    // Add like to the state
+    const newLike = state.likes.addLike(
+      currentID,
+      state.recipe.title,
+      state.recipe.author,
+      state.recipe.img
+    ) 
+
+    // Toggle the like button
+    
+
+    // Add like to UI list
+    console.log(state.likes)
+      
+  // User has liked the current recipe
+  } else {
+    // Remove like to the state
+    state.likes.deleteLike(currentID)
+
+    // Toggle the like button
+
+    // Remove like to UI list
+    console.log(state.likes)
+  }
+}
+
 
 // Handling recipe button clicks
 elements.recipe.addEventListener("click", (e) => {
@@ -149,7 +184,11 @@ elements.recipe.addEventListener("click", (e) => {
     state.recipe.updateServings("inc")
     recipeView.updateServingsIngredients(state.recipe)
   } else if (e.target.matches('.recipe__btn--add, .recipe__btn--add *')) {
+    // Add ingredients to shopping list
     controlList()
+  } else if (e.target.matches('.recipe__love, .recipe__love *')) {
+    // Like controller
+    controlLike()
   }
 })
 
